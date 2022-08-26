fddf4289e56c6c7ad1b443b093e39c922c868f99
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2022-08-26 11:46:08 +0530

    Update Dance website learn how to store contact data in db

--- DanceWebsite/app.js
+++ DanceWebsite/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const path = require('path');
+var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/contactDanceDB',{useNewUrlParser:true});
 
 const app = express();
 const port = 80;
+
+// Define mongoose schema
+var contactSchema = new mongoose.Schema({
+    name:String,
+    phone:String,
+    email:String,
+    address:String,
+    gender:String,
+    reason:String
+});
+
+var Contact = mongoose.model('contact',contactSchema)
 
 //- EXPRESS Specific configuration 
 app.use('/static',express.static('static'));
@@ -22,6 +38,15 @@ app.get('/',(req,res)=>{
 app.get('/contact',(req,res)=>{
     const var1 = {}
     res.status(200).render('contact.pug',var1);
+})
+
+app.post('/contact',(req,res)=>{
+    var userData = new Contact(req.body);
+    userData.save().then(()=>{
+        res.send("Registerd Successfully Wait for The Response.Thankyou!");
+    }).catch(()=>{
+        res.status(404).send("Opps! Sorry.Try Again");
+    });
 })
 
 //- Start the server
