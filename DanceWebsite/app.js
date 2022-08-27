2bc55c770f472cff9f8f92db6142b1c470b5a308
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2022-08-27 17:43:58 +0530

    hosted

--- DanceWebsite/app.js
+++ DanceWebsite/app.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const path = require('path');
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/contactDanceDB',{useNewUrlParser:true});
+// mongoose.connect('mongodb://localhost/contactDanceDB',{useNewUrlParser:true});
 
 const app = express();
-const port = 80;
+const port = 800;
 
 // Define mongoose schema
-var contactSchema = new mongoose.Schema({
-    name:String,
-    phone:String,
-    email:String,
-    address:String,
-    gender:String,
-    reason:String
-});
+// var contactSchema = new mongoose.Schema({
+//     name:String,
+//     phone:String,
+//     email:String,
+//     address:String,
+//     gender:String,
+//     reason:String
+// });
 
-var Contact = mongoose.model('contact',contactSchema)
+// var Contact = mongoose.model('contact',contactSchema)
 
 //- EXPRESS Specific configuration 
 app.use('/static',express.static('static'));
@@ -40,14 +40,14 @@ app.get('/contact',(req,res)=>{
     res.status(200).render('contact.pug',var1);
 })
 
-app.post('/contact',(req,res)=>{
-    var userData = new Contact(req.body);
-    userData.save().then(()=>{
-        res.send("Registerd Successfully Wait for The Response.Thankyou!");
-    }).catch(()=>{
-        res.status(404).send("Opps! Sorry.Try Again");
-    });
-})
+// app.post('/contact',(req,res)=>{
+//     var userData = new Contact(req.body);
+//     userData.save().then(()=>{
+//         res.send("Registerd Successfully Wait for The Response.Thankyou!");
+//     }).catch(()=>{
+//         res.status(404).send("Opps! Sorry.Try Again");
+//     });
+// })
 
 //- Start the server
 app.listen(port,()=>{
