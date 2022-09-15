c6240af4d16306648651573c735ad9a32d0163e4
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2022-09-16 04:01:47 +0530

    update

--- E-CommerceWebsite1/js/script.js
+++ E-CommerceWebsite1/js/script.js
@@ -14,3 +14,24 @@ if(cls){
     })
 }
 
+
+
+
+
+
+// **********Single Page product script ***********
+var Main = document.getElementById('MainImg');
+var small = document.getElementsByClassName('small-img');
+
+small[0].onclick = function(){
+    Main.src = small[0].src;
+}
+small[1].onclick = function(){
+    Main.src = small[1].src;
+}
+small[2].onclick = function(){
+    Main.src = small[2].src;
+}
+small[3].onclick = function(){
+    Main.src = small[3].src;
+}
